023a155f0a1a3d28bfee05790888f66c9c465d02
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2023-02-06 11:12:48 +0800

    delete useless comment

--- source/main.cpp
+++ source/main.cpp
@@ -9,11 +9,8 @@
 void RunMain(HMODULE *phModule)
 {
 	// New Console
-	AllocConsole();
-	freopen_s((FILE **)stdin, "CONIN$", "r", stdin);
-	freopen_s((FILE **)stdout, "CONOUT$", "w", stdout);
-	freopen_s((FILE **)stderr, "CONOUT$", "w", stderr);
-	SetConsoleOutputCP(CP_UTF8);
+	
+	
 
 	DebuggerBypassPre();
 
